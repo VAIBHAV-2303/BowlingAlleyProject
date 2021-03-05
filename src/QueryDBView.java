9d0ea231192cfe7f6b89e0188e8c2ccb0dc4d895
Vaibhav Garg <gargvaibav@gmail.com>
2021-03-05 18:04:32 +0530

    Added common query buttons

--- src/QueryDBView.java
+++ src/QueryDBView.java
@@ -3,13 +3,11 @@ import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.ResultSet;
-import java.util.Vector;
 
 public class QueryDBView implements ActionListener {
 
     private JFrame win;
-    private JButton execute, abort;
+    private JButton execute, highestScore, lowestScore, playerRanks;
     private JLabel queryLabel;
     private JTextField queryField;
 
@@ -23,33 +21,33 @@ public class QueryDBView implements ActionListener {
         JPanel colPanel = new JPanel();
         colPanel.setLayout(new BorderLayout());
 
-        // Patron Panel
-        JPanel patronPanel = new JPanel();
-        patronPanel.setLayout(new GridLayout(3, 1));
-        patronPanel.setBorder(new TitledBorder("Ad-Hoc Query"));
+        JPanel queryPanel = new JPanel();
+        queryPanel.setLayout(new GridLayout(2, 1));
+        queryPanel.setBorder(new TitledBorder("Query"));
 
-        JPanel nickPanel = new JPanel();
-        nickPanel.setLayout(new FlowLayout());
+        JPanel commonQueryPanel = new JPanel();
+        commonQueryPanel.setBorder(new TitledBorder("Common Queries"));
+        commonQueryPanel.setLayout(new FlowLayout());
+        highestScore = ViewUtils.createAndAddPanel("Highest Score", this, commonQueryPanel);
+        lowestScore = ViewUtils.createAndAddPanel("Lowest Score", this, commonQueryPanel);
+        playerRanks = ViewUtils.createAndAddPanel("Player Ranks", this, commonQueryPanel);
+
+        JPanel adHocQueryPanel = new JPanel();
+        adHocQueryPanel.setBorder(new TitledBorder("Ad-Hoc Query"));
+        adHocQueryPanel.setLayout(new FlowLayout());
         queryLabel = new JLabel("Enter Query");
-        queryField = new JTextField("", 15);
-        nickPanel.add(queryLabel);
-        nickPanel.add(queryField);
+        queryField = new JTextField("select * from scores;", 25);
+        adHocQueryPanel.add(queryLabel);
+        adHocQueryPanel.add(queryField);
+        execute = ViewUtils.createAndAddPanel("Execute", this, adHocQueryPanel);
 
-        patronPanel.add(nickPanel);
-
-        // Button Panel
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(4, 1));
-
-        execute = ViewUtils.createAndAddPanel("Execute", this, buttonPanel);
-        abort = ViewUtils.createAndAddPanel("Abort", this, buttonPanel);
+        queryPanel.add(commonQueryPanel);
+        queryPanel.add(adHocQueryPanel);
 
         // Clean up main panel
-        colPanel.add(patronPanel, "Center");
-        colPanel.add(buttonPanel, "East");
+        colPanel.add(queryPanel, "Center");
 
         win.getContentPane().add("Center", colPanel);
-
         win.pack();
 
         // Center Window on Screen
@@ -62,12 +60,24 @@ public class QueryDBView implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource().equals(abort)) {
-            win.setVisible(false);
-        }
         if (e.getSource().equals(execute)) {
             win.setVisible(false);
             String query = queryField.getText();
+            QueryResultView queryResultView = new QueryResultView(ScoreHistorySQL.executeAdHoc(query));
+        }
+        if (e.getSource().equals(highestScore)) {
+            win.setVisible(false);
+            String query = "select max(score) from scores;";
+            QueryResultView queryResultView = new QueryResultView(ScoreHistorySQL.executeAdHoc(query));
+        }
+        if (e.getSource().equals(lowestScore)) {
+            win.setVisible(false);
+            String query = "select min(score) from scores;";
+            QueryResultView queryResultView = new QueryResultView(ScoreHistorySQL.executeAdHoc(query));
+        }
+        if (e.getSource().equals(playerRanks)) {
+            win.setVisible(false);
+            String query = "select nick, avg(score) from scores group by nick order by score desc;";
             QueryResultView queryResultView = new QueryResultView(ScoreHistorySQL.executeAdHoc(query));
         }
     }
