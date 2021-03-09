ff97f74cfb5f4a33beb9407612497afcb7c50158
Surekha_AI <47828289+surekhamedapati@users.noreply.github.com>
2021-03-09 23:21:35 +0530

    Update NumConfig.java

--- src/NumConfig.java
+++ src/NumConfig.java
@@ -2,6 +2,6 @@
 
 public class NumConfig {
 	public static final int numLanes = 3;
-	public static final int maxPatronsPerParty=5;
+	public static final int maxPatronsPerParty=6;
 	public static final int POINTS_PER_INCH = 72;
 }
